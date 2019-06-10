98edc8e218b8ec4c545841dbe7315f601ef20e60
yagya <yagyaprakash3@gmail.com>
2019-06-10 18:09:52 +0530

    full tested

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
        
-        
+        print("this is full tested:")
      
     }
 
