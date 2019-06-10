4bb290ea663035ac878e7d6a2a738d884908eefd
yagya <yagyaprakash3@gmail.com>
2019-06-10 18:06:04 +0530

    add full test

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -14,8 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
        
-        print("Add work here")
-     
+      
+     print("Full test")
     }
 
 
