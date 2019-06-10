53aa83bf66ba396c3998f3accb7f11c9fc50e4fc
yagya <yagyaprakash3@gmail.com>
2019-06-10 18:15:12 +0530

    deleted print completely

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -14,7 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
        
-        print("this is full tested:")
+        
+        
      
     }
 
